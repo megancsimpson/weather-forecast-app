4969b0306bbd2accb25f3d61938b0ad9b32e1d8f
megancsimpson <megan@simpsonclan.org>
2026-08-31 16:46:07 -0700

    Footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,19 +4,29 @@ function Footer() {
       <p>© 2026 Megan Simpson</p>
       <p>Software Development Student</p>
       <p>Built with React, Vite, CSS, and the OpenWeatherMap API</p>
+      <p>
+        Weather data provided by{' '}
+        <a
+          href="https://openweathermap.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          OpenWeather
+        </a>
+      </p>
       <a
         href="https://www.linkedin.com/in/megancsimpson"
         target="_blank"
         rel="noreferrer"
       >
-          LinkedIn
+        LinkedIn
       </a>
       <a
         href="https://github.com/megancsimpson/weather-forecast-app"
         target="_blank"
         rel="noreferrer"
       >
-          GitHub
+        GitHub
       </a>
     </footer>
   )
